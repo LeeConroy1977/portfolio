2aab2d495945c9b92db4ef1dbf762052cb0fd6bc
LeeConroy1977 <leejconroy@gmail.com>
2024-10-15 21:31:53 +0100

    main routes added to app.jsx and applayout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import AppLayout from "./layout/AppLayout";
+
 function App() {
-  return <div>app</div>;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route element={<AppLayout />}>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div className="w-full h-[9%] bg-green-300">NavBar</div>;
+};
+
+export default NavBar;
